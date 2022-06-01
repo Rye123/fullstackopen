c0deaaa982c9ae3d48447cacb74b6eaa841c0755
Ryan Toh <Rye123@users.noreply.github.com>
2022-06-01 22:10:47 +0800

    Completed ex2.6 (adding names)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,46 @@
 import { useState } from 'react'
 
+const Entry = ({person}) => {
+  return (
+    <div>
+      {person.name}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) ;
   const [newName, setNewName] = useState('');
 
+  const handleAddToPhonebook = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddToPhonebook}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => {
+        return <Entry key={person.name} person={person} />
+      })}
     </div>
   )
 };
