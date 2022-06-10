501af92289ada5b1497a500e7a37fb0ea3109c97
Ryan Toh <Rye123@users.noreply.github.com>
2022-06-10 20:15:04 +0800

    refactor: form, listing, entry out of App

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,15 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const Entry = ({person}) => {
-  return (
-    <div>
-      {person.name}
-    </div>
-  )
-}
+import Entry from './components/Entry';
 
-const App = () => {
-  const [persons, setPersons] = useState([]) ;
+const PhonebookForm = ({handlePhonebookFormSubmission}) => {
   const [newName, setNewName] = useState('');
-  
-  // fetch initial state
-  useEffect(() => {
-    axios
-    .get("http://localhost:3001/persons")
-    .then(response => {
-      setPersons(response.data);
-    })
-  }, []);
-  
-  // EVENTS
-  const handleAddToPhonebook = (event) => {
-    event.preventDefault();
-    const newPerson = {
-      name: newName
-    }
-    setPersons(persons.concat(newPerson))
-  }
 
   return (
-    <div>
+    <>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddToPhonebook}>
+      <form onSubmit={handlePhonebookFormSubmission}>
         <div>
           name: 
           <input 
@@ -46,10 +21,46 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+    </>
+  )
+}
+
+const PhonebookListing = ({persons}) => {
+  return (
+    <>
       <h2>Numbers</h2>
       {persons.map(person => {
         return <Entry key={person.name} person={person} />
       })}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) ;
+
+  // EVENTS
+  const handleAddToPhonebook = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: event.target[0].value
+    }
+    setPersons(persons.concat(newPerson))
+  }
+  
+  // fetch initial state
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      setPersons(response.data);
+    })
+  }, []);
+
+  return (
+    <div>
+      <PhonebookForm handlePhonebookFormSubmission={handleAddToPhonebook}/>
+      <PhonebookListing persons={persons}/>
     </div>
   )
 };
---
+++ part2/phonebook/src/components/Entry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Entry = ({person}) => {
+    return (
+        <div>
+            {person.name}
+        </div>
+    );
+};
+
+export default Entry;
+
